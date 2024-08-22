058488817c311830925ddf6e5b48ac105c5e61c3
Dimitar <dimitar.mandarliev@gmail.com>
2024-08-22 13:27:03 +0300

    Auth screens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
-import { Button } from "@/components/ui/button";
+import { AuthScreen } from "@/features/auth/components/auth-screen";
 
 export default function Home() {
-  return (
-    <div className="text-rose-500 font-bold text-2xl">
-      <Button variant="destructive">Click Me</Button>
-    </div>
-  );
+  return <AuthScreen />;
 }
