61f46a3faca5887477369ba4bdbaf8195a69bb86
Dimitar <dimitar.mandarliev@gmail.com>
2024-09-13 13:28:37 +0300

    input

---
+++ src/app/workspace/[workspaceId]/channel/[channelId]/chat-input.tsx
@@ -0,0 +1,11 @@
+import Editor from "@/components/editor";
+
+const ChatInput = () => {
+  return (
+    <div className="px-5 w-full">
+      <Editor />
+    </div>
+  );
+};
+
+export default ChatInput;
